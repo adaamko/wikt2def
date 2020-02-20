22c0f8f4b9fed7e9a58c02d09e65dcb96ab3e05a
Adam Kovacs <adaamko@nessie.ilab.sztaki.hu>
2020-02-20 13:49:18 +0100

    added utils

---
+++ scripts/utils.py
@@ -0,0 +1,7 @@
+
+def get_distance(src_word, tgt_word, src_emb, tgt_emb):
+    src_word_emb = src_emb[src_word2id[src_word]]
+    tgt_word_emb = tgt_emb[tgt_word2id[tgt_word]]
+    score = (tgt_word_emb / np.linalg.norm(tgt_word_emb)).dot(src_word_emb / np.linalg.norm(src_word_emb))
+    return score
+    
