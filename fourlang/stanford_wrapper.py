7ccff3e241ab7f6802257862b3aeac9b140d1d20
Adam Kovacs <adaamko@nessie.ilab.sztaki.hu>
2020-03-02 10:34:59 +0100

    Added caching

--- fourlang/stanford_wrapper.py
+++ fourlang/stanford_wrapper.py
@@ -3,22 +3,39 @@ import re
 import sys
 import requests
 import json
+import networkx as nx
 
 
 class StanfordParser():
     
     def __init__(self, port=5005):
         self.server = "http://127.0.0.1:" + str(port)
+        self.parse = {}
         
-    def parse_text(self, text):
-        data = {'text': text}
-        data_json = json.dumps(data)
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.post(self.server + "/parse", data=data_json, headers=headers)
+    def parse_text(self, text, word=None):
+        if word and word in self.parse:
+            deps = self.parse[word]
+        else:
+            data = {'text': text}
+            data_json = json.dumps(data)
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            r = requests.post(self.server + "/parse", data=data_json, headers=headers)
 
-        deps = r.json()["deps"]
+            deps = r.json()["deps"]
+            if word:
+                self.parse[word] = deps
+
         corefs = []
         return deps, corefs
+
+    def load_from_dict(self):
+        with open("def_parses", "r") as f:
+            self.parse = json.load(f)
+
+    def save_dict(self):
+        with open("def_parses", "w+") as f:
+            dict_json = json.dumps(self.parse)
+            f.write(dict_json)
 
     def lemmatize_text(self, text):
         data = {'text': text}
